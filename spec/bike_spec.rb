52a67edce79dcf187f54c9dd7f0a6a400a8c373c
Will <w_birdseye@hotmail.co.uk>
2021-10-19 17:36:19 +0100

    test for working method failed

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -4,3 +4,4 @@ describe Bike do
     new_bike = Bike.new
    it { expect(new_bike).to respond_to(:working?) }
 end
+
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -3,5 +3,6 @@ require 'dockingstation'
 describe DockingStation do 
    docking_station = DockingStation.new
    it { expect(docking_station).to respond_to(:release_bike) }
+   it { expect(docking_station.release_bike).to respond_to(:working?) }
 end
 
