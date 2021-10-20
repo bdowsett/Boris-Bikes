f4d2faef33a6d4d74c18a05914a92696400ded30
Ben Dowsett <bendowsett@gmail.com>
2021-10-20 15:01:24 +0100

    test for working? method written and passed

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -3,5 +3,6 @@ require 'bike'
 describe Bike do 
     new_bike = Bike.new
    it { expect(new_bike).to respond_to(:working?) }
+   it { expect(new_bike.working?).to eq(true) }
 end
 
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -4,5 +4,6 @@ describe DockingStation do
    docking_station = DockingStation.new
    it { expect(docking_station).to respond_to(:release_bike) }
    it { expect(docking_station.release_bike).to respond_to(:working?) }
+   it { expect(docking_station.release_bike.working?).to eq true }
 end
 
