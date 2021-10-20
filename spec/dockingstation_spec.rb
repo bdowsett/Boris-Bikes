a1778806c45c168dd3b2ba7e7f5d36bd665946c1
Ben Dowsett <bendowsett@gmail.com>
2021-10-20 15:21:35 +0100

    Dock bike test written and passed

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -2,8 +2,11 @@ require 'dockingstation'
 
 describe DockingStation do 
    docking_station = DockingStation.new
+   new_bike = docking_station.release_bike
    it { expect(docking_station).to respond_to(:release_bike) }
-   it { expect(docking_station.release_bike).to respond_to(:working?) }
-   it { expect(docking_station.release_bike.working?).to eq true }
+   it { expect(new_bike).to respond_to(:working?) }
+   it { expect(new_bike.working?).to eq true }
+   it { expect(docking_station.dock_bike(new_bike)).to eq true}
+   it { expect(docking_station.dock_bike("new bike")).to eq false}
 end
 
