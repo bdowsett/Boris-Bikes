1fb6ddac85019c01de99e6b4994d4f452c54dd06
Matt Zimmer <zimmermja@gmail.com>
2021-10-20 17:27:09 +0100

    Chapter 13 - error message added for unsuccessful docking

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -3,11 +3,12 @@ require_relative 'bike'
 class DockingStation
 
     def release_bike
-      fail "There are no bikes" unless @bike 
+        fail "There are no bikes" unless @bike 
         @bike
     end
 
     def dock_bike(mybike)
+        fail "There are already bikes docked" if @bike 
         @bike = mybike
     end
 
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -3,11 +3,15 @@ require 'dockingstation'
 describe DockingStation do 
    docking_station = DockingStation.new
    new_bike = Bike.new
-   
 
-   it "matches the error message" do
+   it "Expects an error when there ARE NOT bikes docked" do
     expect { subject.release_bike }.to raise_error("There are no bikes")
-   end 
+   end
+
+   it "Expects an error when there ARE bikes docked" do
+      subject.dock_bike(Bike.new)
+      expect { subject.dock_bike(Bike.new) }.to raise_error("There are already bikes docked")
+   end
   
    it { expect(docking_station).to respond_to(:release_bike) }
    it { expect(new_bike).to respond_to(:working?) }
@@ -20,20 +24,5 @@ describe DockingStation do
       expect(subject.dock_bike(bike)).to eq bike
 
    end
-
- #  it { expect(docking_station.dock_bike(new_bike)).to eq 1}
- #  it { expect(docking_station.dock_bike("new bike")).to eq 0}
- #  it { expect(docking_station.see_bikes[0].class).to eq Bike}
-
-=begin
- condition = subject.see_bike
- context "equals nil", if: condition.nil? do 
-    it "return error" do 
-    end 
-    context "does not equal nil", if: !condition.nil? do 
-       it "does not return error" do 
-       end  
- end 
-=end 
 end 
 
